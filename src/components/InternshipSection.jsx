e3cdcaeb3cc4682660d9fe55b6362fc905717345
Antoni <antek.erens@icloud.com>
2026-04-07 20:49:12 +0200

    aaaaa

--- src/components/InternshipSection.jsx
+++ src/components/InternshipSection.jsx
@@ -1,14 +1,20 @@
-export default function InternshipSection({ number, title, description, screenshot, kolor }) {
+export default function InternshipSection({ number, title, description, screenshot, kolor, tekstkolor}) {
   return (
     <section 
       className="min-h-screen w-full flex flex-col items-center justify-center text-left"
       style={{ backgroundColor: kolor }}
     >
-      <h2 className="font-serif text-[50px] text-[#111] mb-6">
+      <h2 
+        className="font-serif text-[50px] mb-6" 
+        style={{ color: tekstkolor }}
+      >
         {title}
       </h2>
       
-      <p className="font-serif text-2xl text-[#333] max-w-[1250px] leading-[1.2]">
+      <p 
+        className="font-serif text-2xl max-w-[1250px] leading-[1.2]" 
+        style={{ color: tekstkolor }}
+      >
         {description}
       </p>
 
